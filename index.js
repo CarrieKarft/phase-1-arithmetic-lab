4dc4cb7a7428ff34fdbdbd37a8714c7b3c8bbb20
Carrie Kraft <carriekarft@gmail.com>
2022-11-23 16:18:43 -0800

    commit tree 22201637ba5bb25a64f01a3c4c451acb98e4eafe

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+
+let multiply = num1 * num2;
+
+const random = Math.round(Math.random() + 1);
+
+const num3 = 28
+const num4 = 6
+
+let mod = num3 % num4;
+
+let max = Math.max(2, 4, 8, 10, 20);
